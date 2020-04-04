14ff373415b09630777bd7c04a029bd99ed60e3e
ramzimh <ramzimahnoune@gmail.com>
2020-04-04 14:49:54 +0100

    adding the compose page

--- app.js
+++ app.js
@@ -19,6 +19,21 @@ app.get("/",function(req,res){
   res.render("home.ejs",{homeStart:homeStartingContent});
 })
 
+app.get("/about",function(req,res){
+  res.render("about.ejs",{aboutContent:aboutContent});
+})
+
+app.get("/contact",function(req,res){
+  res.render("contact.ejs",{contactContent:contactContent});
+})
+
+app.get("/compose",function(req,res){
+  res.render("compose.ejs");
+})
+
+
+
+
 
 
 
