60621002f11b39637b7fb56cf354d6fbb5c3b084
ramzimh <ramzimahnoune@gmail.com>
2020-04-05 17:40:33 +0100

    fix some url issues

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _=require("lodash");
 var posts = [];
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -17,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render("home.ejs", { homeStart: homeStartingContent, posts:posts });
+  res.render("home.ejs", { homeStart: homeStartingContent, posts: posts });
 })
 
 app.get("/about", function (req, res) {
@@ -42,18 +43,18 @@ app.post("/compose", function (req, res) {
   res.redirect("/");
 });
 
-app.get("/posts/:postName",function(req,res){
-const requestedTitle=req.params.postName.toLowerCase;
-var titleExist=false;
-posts.forEach(function(pst){
-  if (pst.title.toLowerCase===requestedTitle){
-    titleExist=true;
-   res.render("post.ejs",{title:pst.title, content:pst.content});
-  }else{
-    console.log("nope it doesn't exist");
-    
-  }
-})
+app.get("/posts/:postName", function (req, res) {
+  const requestedTitle = _.lowerCase(req.params.postName);
+  var titleExist = false;
+  posts.forEach(function (pst) {
+    if (_.lowerCase(pst.title) === requestedTitle) {
+      titleExist = true;
+      res.render("post.ejs", { title: pst.title, content: pst.content });
+    } else {
+      console.log("nope it doesn't exist");
+
+    }
+  })
 
 });
 
