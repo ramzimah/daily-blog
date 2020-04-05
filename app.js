bd8011978354022fb4d914dfe599227a1d87c7e0
ramzimh <ramzimahnoune@gmail.com>
2020-04-05 12:29:52 +0100

    add dynamic url for posts

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+var posts = [];
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -12,31 +13,49 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/",function(req,res){
-  res.render("home.ejs",{homeStart:homeStartingContent});
+app.get("/", function (req, res) {
+  res.render("home.ejs", { homeStart: homeStartingContent, posts:posts });
 })
 
-app.get("/about",function(req,res){
-  res.render("about.ejs",{aboutContent:aboutContent});
+app.get("/about", function (req, res) {
+  res.render("about.ejs", { aboutContent: aboutContent });
 })
 
-app.get("/contact",function(req,res){
-  res.render("contact.ejs",{contactContent:contactContent});
+app.get("/contact", function (req, res) {
+  res.render("contact.ejs", { contactContent: contactContent });
 })
 
-app.get("/compose",function(req,res){
+app.get("/compose", function (req, res) {
   res.render("compose.ejs");
 })
 
-app.post("/compose",function(req,res){
-  var enteredPub=req.body.pub;
-  console.log(enteredPub);
-  
+
+app.post("/compose", function (req, res) {
+  var post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  }
+  posts.push(post);
+  res.redirect("/");
+});
+
+app.get("/posts/:postName",function(req,res){
+const requestedTitle=req.params.postName.toLowerCase;
+var titleExist=false;
+posts.forEach(function(pst){
+  if (pst.title.toLowerCase===requestedTitle){
+    titleExist=true;
+   res.render("post.ejs",{title:pst.title, content:pst.content});
+  }else{
+    console.log("nope it doesn't exist");
+    
+  }
 })
 
+});
 
 
 
@@ -47,10 +66,6 @@ app.post("/compose",function(req,res){
 
 
 
-
-
-
-
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
